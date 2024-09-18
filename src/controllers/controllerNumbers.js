8c3a75a1eda3f9a45e3664311e4b27dcc25036ba
anaesparapani <159201364+anaesparapani@users.noreply.github.com>
2024-09-18 12:50:06 -0300

    controllerNumbers.js

--- src/controllers/controllerNumbers.js
+++ src/controllers/controllerNumbers.js
@@ -2,6 +2,10 @@ module.exports = class controllerNumbers{
     static async checkPar(req, res){
         const {numero} = req.body;
 
+        if(typeof numero !== "number"){
+            return res.status(400).json({message: "Não é um número!!!"});
+        }
+
         if(numero % 2 == 0){
             return res.status(200).json({message: "Número Par!"});
         }
